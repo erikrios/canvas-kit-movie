14df1e8d344725bd89c2d4db51c02d0e958c1291
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-25 21:32:27 +0700

    Create TvShow.kt

---
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/model/TvShow.kt
@@ -0,0 +1,21 @@
+package io.erikrios.github.canvaskitmovie.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class TvShow(
+    val id: Int,
+    val backdropPath: String? = null,
+    val genres: List<Genre>? = null,
+    val originalLanguage: String,
+    val originalName: String,
+    val name: String,
+    val overview: String,
+    val popularity: Double,
+    val posterPath: String? = null,
+    val firstAirDate: String,
+    val status: String? = null,
+    val voteAverage: Double,
+    val voteCount: Int
+) : Parcelable

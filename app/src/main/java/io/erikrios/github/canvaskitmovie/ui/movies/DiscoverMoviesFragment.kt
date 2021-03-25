50d027838832a723e502ccce704aaa38643e247a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-25 12:26:43 +0700

    Create DiscoverMoviesFragment.kt

---
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/movies/DiscoverMoviesFragment.kt
@@ -0,0 +1,27 @@
+package io.erikrios.github.canvaskitmovie.ui.movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.erikrios.github.canvaskitmovie.databinding.FragmentDiscoverMoviesBinding
+
+class DiscoverMoviesFragment : Fragment() {
+
+    private var _binding: FragmentDiscoverMoviesBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDiscoverMoviesBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

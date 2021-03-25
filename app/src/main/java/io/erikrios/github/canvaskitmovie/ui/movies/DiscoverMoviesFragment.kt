116322734ce08fd28327360f4b6c194d90cd4223
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-25 20:44:59 +0700

    Generate dummy movies

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/movies/DiscoverMoviesFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/movies/DiscoverMoviesFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.databinding.FragmentDiscoverMoviesBinding
 import io.erikrios.github.canvaskitmovie.ui.adapter.MovieAdapter
+import io.erikrios.github.canvaskitmovie.utils.DummyData.generateMovies
 
 class DiscoverMoviesFragment : Fragment() {
 
@@ -26,7 +27,8 @@ class DiscoverMoviesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setRecyclerView(generateMovies())
+        val movies = generateMovies()
+        setRecyclerView(movies)
     }
 
     override fun onDestroyView() {
@@ -39,29 +41,5 @@ class DiscoverMoviesFragment : Fragment() {
             Toast.makeText(context, movie.originalTitle, Toast.LENGTH_SHORT).show()
         }
         binding?.rvMovies?.adapter = adapter
-    }
-
-    private fun generateMovies(): List<Movie> {
-        val movies = mutableListOf<Movie>()
-        for (i in 0..50) {
-            val posterPath =
-                if (i % 2 == 0) "/lPsD10PP4rgUGiGR4CCXA6iY0QQ.jpg" else "/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg"
-            movies.add(
-                Movie(
-                    id = i,
-                    adult = true,
-                    originalLanguage = "en",
-                    title = if (i % 2 == 0) "Raya and the Last Dragon $i" else "Zack Snyder's Justice League $i",
-                    originalTitle = "Zack Snyder's Justice League $i",
-                    overview = "Determined $i to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.",
-                    popularity = 11783.352,
-                    posterPath = posterPath,
-                    releaseDate = "2021-03-18",
-                    voteAverage = if (i % 2 == 0) 10.0 else 4.2,
-                    voteCount = 3389
-                )
-            )
-        }
-        return movies
     }
 }

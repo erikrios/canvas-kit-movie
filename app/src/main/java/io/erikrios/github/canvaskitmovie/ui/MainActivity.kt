0f0f638d404575b95a11262f8fb6bdb4104cab1a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-25 11:11:51 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package io.erikrios.github.canvaskitmovie
+package io.erikrios.github.canvaskitmovie.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.canvaskitmovie.R
 import io.erikrios.github.canvaskitmovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {

7ffa4c5a79a61d835342458f7dd5b88567902346
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-26 11:38:19 +0700

    Remove unnecessary code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/dashboard/DashboardFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import io.erikrios.github.canvaskitmovie.R
 import io.erikrios.github.canvaskitmovie.databinding.FragmentDashboardBinding
@@ -29,16 +26,6 @@ class DashboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val navController = requireActivity().findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.item_movies,
-                R.id.item_tv_shows
-            )
-        )
-        (activity as AppCompatActivity).setupActionBarWithNavController(
-            navController,
-            appBarConfiguration
-        )
         binding?.bottomNavigation?.setupWithNavController(navController)
     }
 

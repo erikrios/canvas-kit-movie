1b1bf1a27d8cd086c9f621e8ccbc2dccfa1d3487
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-26 12:26:35 +0700

    Load the image view with glide

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import io.erikrios.github.canvaskitmovie.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
@@ -21,6 +22,15 @@ class DetailsFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.let {
+            Glide.with(requireContext())
+                .load("https://image.tmdb.org/t/p/original${args.movie.backdropPath}")
+                .into(it.imgPoster)
+        }
     }
 
     override fun onDestroyView() {

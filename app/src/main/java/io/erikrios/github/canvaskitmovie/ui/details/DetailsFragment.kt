4585e6e5c81768fd9473898c08236a23feb185f9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-26 10:20:02 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
@@ -1,6 +1,23 @@
 package io.erikrios.github.canvaskitmovie.ui.details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.erikrios.github.canvaskitmovie.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
 }

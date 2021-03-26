35dccd54f7edc1636879ef571c32281fe4c4f636
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-26 13:22:29 +0700

    Integrated the view

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import io.erikrios.github.canvaskitmovie.R
+import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
@@ -26,15 +30,26 @@ class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.let {
-            Glide.with(requireContext())
-                .load("https://image.tmdb.org/t/p/original${args.movie.backdropPath}")
-                .into(it.imgPoster)
-        }
+        handleView(args.movie)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun handleView(movie: Movie) {
+        binding?.apply {
+            Glide.with(requireContext())
+                .load("https://image.tmdb.org/t/p/original${args.movie.backdropPath}")
+                .into(imgPoster)
+            toolbar.apply {
+                title = movie.title
+                navigationIcon =
+                    ContextCompat.getDrawable(context, R.drawable.ic_baseline_arrow_back_24)
+                setNavigationOnClickListener { findNavController().popBackStack() }
+            }
+        }
+
     }
 }

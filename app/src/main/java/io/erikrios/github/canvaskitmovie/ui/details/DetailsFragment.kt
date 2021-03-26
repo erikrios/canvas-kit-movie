b9c2f532316a3589a04b45d1f6684038a1607491
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-26 10:43:27 +0700

    Hide support action bar when onCreateView and hide support action bar when onDestroyView

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/details/DetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import io.erikrios.github.canvaskitmovie.databinding.FragmentDetailsBinding
@@ -21,5 +22,16 @@ class DetailsFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as AppCompatActivity).supportActionBar?.show()
+        _binding = null
     }
 }

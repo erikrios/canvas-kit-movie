ec86be2f54345e456952ffe940164b66cd4a2de3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-28 10:53:49 +0700

    Implements the recycler view

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/tvshows/DiscoverTvShowsFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/tvshows/DiscoverTvShowsFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import io.erikrios.github.canvaskitmovie.data.model.TvShow
 import io.erikrios.github.canvaskitmovie.databinding.FragmentDiscoverTvShowsBinding
+import io.erikrios.github.canvaskitmovie.ui.adapter.CinemaAdapter
+import io.erikrios.github.canvaskitmovie.utils.DummyData.generateTvShows
 
 class DiscoverTvShowsFragment : Fragment() {
 
     private var _binding: FragmentDiscoverTvShowsBinding? = null
     private val binding get() = _binding
+    private lateinit var adapter: CinemaAdapter<TvShow>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +25,25 @@ class DiscoverTvShowsFragment : Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tvShows = generateTvShows()
+        setRecyclerView(tvShows)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setRecyclerView(tvShows: List<TvShow>) {
+        adapter = CinemaAdapter(tvShows) { tvShow ->
+            val action =
+                DiscoverTvShowsFragmentDirections.actionDiscoverTvShowsFragmentToTvShowDetailsFragment(
+                    tvShow
+                )
+            findNavController().navigate(action)
+        }
+        binding?.rvTvShows?.adapter = adapter
     }
 }

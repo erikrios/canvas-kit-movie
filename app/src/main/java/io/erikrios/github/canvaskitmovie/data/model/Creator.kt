697b7891027879ede541d9cf08cc0abaeb3d1325
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-28 12:59:45 +0700

    Create Creator.kt

---
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/model/Creator.kt
@@ -0,0 +1,11 @@
+package io.erikrios.github.canvaskitmovie.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Creator(
+    val id: Long,
+    val name: String,
+    val profilePath: String
+) : Parcelable

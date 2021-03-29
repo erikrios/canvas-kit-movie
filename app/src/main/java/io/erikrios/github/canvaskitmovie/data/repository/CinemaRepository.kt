e5b789a9698ea9aa5f65cbf892ccdb4782c3cdfb
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-29 10:25:29 +0700

    Create CinemaRepository.kt

---
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/repository/CinemaRepository.kt
@@ -0,0 +1,9 @@
+package io.erikrios.github.canvaskitmovie.data.repository
+
+import io.erikrios.github.canvaskitmovie.data.model.Movie
+import io.erikrios.github.canvaskitmovie.data.model.TvShow
+
+interface CinemaRepository {
+    fun getMovies(): List<Movie>
+    fun getTvShows(): List<TvShow>
+}

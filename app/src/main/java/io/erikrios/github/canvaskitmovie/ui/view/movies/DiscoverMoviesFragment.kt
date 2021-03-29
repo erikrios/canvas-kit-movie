14e497ed015581183c7ab0f6a9535ff610d6d0ff
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-29 11:23:32 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/movies/DiscoverMoviesFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/movies/DiscoverMoviesFragment.kt
@@ -11,7 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.databinding.FragmentDiscoverMoviesBinding
 import io.erikrios.github.canvaskitmovie.ui.adapter.CinemaAdapter
-import io.erikrios.github.canvaskitmovie.ui.movies.DiscoverMoviesFragmentDirections
 import io.erikrios.github.canvaskitmovie.ui.viewmodel.MainViewModel
 
 @AndroidEntryPoint

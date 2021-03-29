8aa82cf5f015f7742ccc0048e62b2ca1cd4cd8e8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-29 10:13:16 +0700

    Create CanvasKitMovieApplication.kt

---
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/CanvasKitMovieApplication.kt
@@ -0,0 +1,7 @@
+package io.erikrios.github.canvaskitmovie
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CanvasKitMovieApplication : Application()

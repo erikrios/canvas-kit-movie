bd0522926e8e68656ae3efd50423504a91c02963
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-02 21:31:43 +0700

    Add a method to provide base url

--- app/src/main/java/io/erikrios/github/canvaskitmovie/di/ApplicationModule.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/di/ApplicationModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.erikrios.github.canvaskitmovie.BuildConfig
 import io.erikrios.github.canvaskitmovie.data.repository.CinemaRepository
 import io.erikrios.github.canvaskitmovie.data.repository.CinemaRepositoryImpl
 import javax.inject.Singleton
@@ -11,6 +12,9 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = BuildConfig.BASE_URL
 
     @Singleton
     @Provides

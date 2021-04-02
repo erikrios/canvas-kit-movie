88b5581c92239061f75e8b69ca6ae7b08d54070a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-03 02:13:31 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/data/repository/CinemaRepository.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/repository/CinemaRepository.kt
@@ -2,22 +2,11 @@ package io.erikrios.github.canvaskitmovie.data.repository
 
 import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.data.model.TvShow
-import io.erikrios.github.canvaskitmovie.data.source.DataSource
 import io.erikrios.github.canvaskitmovie.utils.Resource
-import javax.inject.Inject
-import javax.inject.Named
 
-abstract class CinemaRepository {
-    @Inject
-    @Named("localeDataSource")
-    protected lateinit var localeDataSource: DataSource
-
-    @Inject
-    @Named("remoteDataSource")
-    protected lateinit var remoteDataSource: DataSource
-
-    abstract suspend fun getMovies(): Resource<List<Movie>>
-    abstract suspend fun getTvShows(): Resource<List<TvShow>>
-    abstract suspend fun getMovieById(id: Int): Resource<Movie>
-    abstract suspend fun getTvShowById(id: Int): Resource<TvShow>
+interface CinemaRepository {
+    suspend fun getMovies(): Resource<List<Movie>>
+    suspend fun getTvShows(): Resource<List<TvShow>>
+    suspend fun getMovieById(id: Int): Resource<Movie>
+    suspend fun getTvShowById(id: Int): Resource<TvShow>
 }

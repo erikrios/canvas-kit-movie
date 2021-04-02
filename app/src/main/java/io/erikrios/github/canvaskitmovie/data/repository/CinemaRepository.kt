f8faece8c8646cbe3e372f89ca76756b8c48a889
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-03 01:08:23 +0700

    Add locale and remote data source property

--- app/src/main/java/io/erikrios/github/canvaskitmovie/data/repository/CinemaRepository.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/repository/CinemaRepository.kt
@@ -2,10 +2,21 @@ package io.erikrios.github.canvaskitmovie.data.repository
 
 import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.data.model.TvShow
+import io.erikrios.github.canvaskitmovie.data.source.DataSource
+import javax.inject.Inject
+import javax.inject.Named
 
-interface CinemaRepository {
-    fun getMovies(): List<Movie>
-    fun getTvShows(): List<TvShow>
-    fun getMovieById(id: Int): Movie?
-    fun getTvShowById(id: Int): TvShow?
+abstract class CinemaRepository {
+    @Inject
+    @Named("localeDataSource")
+    protected lateinit var localeDataSource: DataSource
+
+    @Inject
+    @Named("remoteDataSource")
+    protected lateinit var remoteDataSource: DataSource
+
+    abstract suspend fun getMovies(): List<Movie>
+    abstract suspend fun getTvShows(): List<TvShow>
+    abstract suspend fun getMovieById(id: Int): Movie?
+    abstract suspend fun getTvShowById(id: Int): TvShow?
 }

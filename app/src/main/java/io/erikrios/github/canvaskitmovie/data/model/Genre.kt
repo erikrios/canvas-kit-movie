9b658e9aacbb118be069157843fa235f6fe359e0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-02 20:15:18 +0700

    Add @SerializedName annotations

--- app/src/main/java/io/erikrios/github/canvaskitmovie/data/model/Genre.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/model/Genre.kt
@@ -1,10 +1,13 @@
 package io.erikrios.github.canvaskitmovie.data.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Genre(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("name")
     val name: String
 ) : Parcelable
--- app/src/main/java/io/erikrios/github/canvaskitmovie/data/model/TvShow.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/model/TvShow.kt
@@ -1,22 +1,37 @@
 package io.erikrios.github.canvaskitmovie.data.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class TvShow(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("backdrop_path")
     val backdropPath: String? = null,
+    @SerializedName("genres")
     val genres: List<Genre>? = null,
+    @SerializedName("original_language")
     val originalLanguage: String,
+    @SerializedName("original_name")
     val originalName: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("overview")
     val overview: String,
+    @SerializedName("popularity")
     val popularity: Double,
+    @SerializedName("poster_path")
     val posterPath: String? = null,
+    @SerializedName("first_air_date")
     val firstAirDate: String,
+    @SerializedName("status")
     val status: String? = null,
+    @SerializedName("vote_average")
     val voteAverage: Double,
+    @SerializedName("vote_count")
     val voteCount: Int,
+    @SerializedName("created_by")
     val creators: List<Creator>
 ) : Parcelable

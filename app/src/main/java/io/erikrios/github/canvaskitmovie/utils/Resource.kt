fcac73c3641160ed124e14fb6191005a06866e49
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-03 00:05:39 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/utils/Resource.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/utils/Resource.kt
@@ -7,9 +7,9 @@ data class Resource<out T>(val status: Status, val data: T?, val message: String
         fun <T> success(data: T?) = Resource(Status.SUCCESS, data, null)
 
 
-        fun <T> error(message: String?) = Resource(Status.ERROR, null, message)
+        fun <T> error(message: String?, data: T?) = Resource(Status.ERROR, data, message)
 
 
-        fun <T> loading(): Resource<T> = Resource(Status.LOADING, null, null)
+        fun <T> loading(data: T?): Resource<T> = Resource(Status.LOADING, data, null)
     }
 }

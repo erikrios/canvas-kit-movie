afcea2cf3e6362b08e79a4006111e3f67b5575da
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-04 13:27:23 +0700

    Test the get movies function

--- app/src/test/java/io/erikrios/github/canvaskitmovie/data/repository/CinemaRepositoryImplTest.kt
+++ app/src/test/java/io/erikrios/github/canvaskitmovie/data/repository/CinemaRepositoryImplTest.kt
@@ -1,15 +1,25 @@
 package io.erikrios.github.canvaskitmovie.data.repository
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import io.erikrios.github.canvaskitmovie.MainCoroutineRule
 import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.data.model.TvShow
+import io.erikrios.github.canvaskitmovie.data.source.local.LocalDataSource
+import io.erikrios.github.canvaskitmovie.data.source.remote.RemoteDataSource
 import io.erikrios.github.canvaskitmovie.utils.DummyData
+import io.erikrios.github.canvaskitmovie.utils.NetworkHelper
+import io.erikrios.github.canvaskitmovie.utils.Resource
+import io.erikrios.github.canvaskitmovie.utils.Status
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
 
 @ExperimentalCoroutinesApi
 class CinemaRepositoryImplTest {
@@ -17,13 +27,20 @@ class CinemaRepositoryImplTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    /**
-     * We use this rule for the next submission, when the Coroutine is needed
-     */
     @get:Rule
     val mainCoroutineRule = MainCoroutineRule()
 
-    private lateinit var repository: CinemaRepository
+    private lateinit var repository: FakeCinemaRepositoryImpl
+
+    @Mock
+    private lateinit var networkHelper: NetworkHelper
+
+    @Mock
+    private lateinit var localDataSource: LocalDataSource
+
+    @Mock
+    private lateinit var remoteDataSource: RemoteDataSource
+
     private lateinit var actualMovies: List<Movie>
     private lateinit var actualTvShows: List<TvShow>
     private var actualMoviesSize: Int = 0
@@ -37,7 +54,8 @@ class CinemaRepositoryImplTest {
 
     @Before
     fun setUp() {
-        repository = CinemaRepositoryImpl()
+        MockitoAnnotations.initMocks(this)
+
         actualMovies = DummyData.generateMovies()
         actualTvShows = DummyData.generateTvShows()
         actualMoviesSize = actualMovies.size
@@ -46,105 +64,116 @@ class CinemaRepositoryImplTest {
         randomTvShowId = actualTvShows.random().id
         actualDummyMovie = DummyData.getMovieById(randomMovieId)
         actualDummyTvShow = DummyData.getTvShowById(randomTvShowId)
+
+        `when`(networkHelper.isNetworkConnected()).thenReturn(true)
+
+        runBlockingTest {
+            `when`(localDataSource.getMovies()).thenReturn(Resource.success(actualMovies))
+            `when`(localDataSource.getTvShows()).thenReturn(Resource.success(actualTvShows))
+            `when`(localDataSource.getMovieDetails(randomMovieId)).thenReturn(
+                Resource.success(
+                    actualDummyMovie
+                )
+            )
+            `when`(localDataSource.getTvShowDetails(randomTvShowId)).thenReturn(
+                Resource.success(
+                    actualDummyTvShow
+                )
+            )
+            `when`(remoteDataSource.getMovies()).thenReturn(Resource.success(actualMovies))
+            `when`(remoteDataSource.getTvShows()).thenReturn(Resource.success(actualTvShows))
+            `when`(remoteDataSource.getMovieDetails(randomMovieId)).thenReturn(
+                Resource.success(
+                    actualDummyMovie
+                )
+            )
+            `when`(remoteDataSource.getTvShowDetails(randomTvShowId)).thenReturn(
+                Resource.success(
+                    actualDummyTvShow
+                )
+            )
+        }
+
+        repository = FakeCinemaRepositoryImpl(networkHelper, localDataSource, remoteDataSource)
     }
 
     /**
      * -------------------- Get movies test ------------------------------
      */
     @Test
-    fun `get movies, returns not null`() {
-        val movies = repository.getMovies()
-        Truth.assertThat(movies).isNotNull()
+    fun `get movies, returns not null`() = runBlockingTest {
+        val moviesResources = repository.getMovies()
+        // Verity that remoteDataSource.getMovies() is called
+        verify(remoteDataSource).getMovies()
+        // Verify that the return value is not null
+        assertThat(moviesResources).isNotNull()
     }
 
     @Test
-    fun `get movies, return the list of movies`() {
-        val movies = repository.getMovies()
-        Truth.assertThat(movies).isNotEmpty()
+    fun `get movies, return the list of movies`() = runBlockingTest {
+        val moviesResources = repository.getMovies()
+        // Verity that remoteDataSource.getMovies() is called
+        verify(remoteDataSource).getMovies()
+        // Verify that the return value are the list of movies
+        assertThat(moviesResources.data).isNotEmpty()
     }
 
     @Test
-    fun `get movies, return the same size with the actual dummy data`() {
-        val movies = repository.getMovies()
-        Truth.assertThat(movies.size).isEqualTo(actualMoviesSize)
+    fun `get movies, return the same size with the actual dummy data`() = runBlockingTest {
+        val moviesResources = repository.getMovies()
+        // Verify that remoteDataSource.getMovies() is called
+        verify(remoteDataSource).getMovies()
+        // Verify that the return value have the same size with the actual dummy data
+        assertThat(moviesResources.data?.size).isEqualTo(actualMoviesSize)
     }
 
     @Test
-    fun `get movies, returns the same elements with the actual dummy data`() {
-        val movies = repository.getMovies()
-        Truth.assertThat(movies).isEqualTo(actualMovies)
+    fun `get movies, returns the same elements with the actual dummy data`() = runBlockingTest {
+        val moviesResources = repository.getMovies()
+        // Verify that remoteDataSource.getMovies() is called
+        verify(remoteDataSource).getMovies()
+        // Verify that the return value have the same element with the actual dummy data
+        assertThat(moviesResources.data).isEqualTo(actualMovies)
     }
 
+    @Test
+    fun `get movies when network connected, return resource instance with success status`() =
+        runBlockingTest {
+            val moviesResources = repository.getMovies()
+            // Verify that remoteDataSource.getMovies() is called
+            verify(remoteDataSource).getMovies()
+            // Verify that the resource instance have success status
+            assertThat(moviesResources.status).isEqualTo(Status.SUCCESS)
+        }
+
+    @Test
+    fun `get movies when network disconnected, return the movies data from local data source`() =
+        runBlockingTest {
+            // Disable the network connection
+            `when`(networkHelper.isNetworkConnected()).thenReturn(false)
+            val movieResource = repository.getMovies()
+            // Verify that localDataSource.getMovies() is called
+            verify(localDataSource).getMovies()
+            // Verity that the return value from local data source is not empty
+            assertThat(movieResource.data).isNotEmpty()
+            // Verity that the status is success
+            assertThat(movieResource.status).isEqualTo(Status.SUCCESS)
+        }
+
+    @Test
+    fun `get movies when network disconnected and the local data source is empty, return error status`() =
+        runBlockingTest {
+            // Disable the network connection
+            `when`(networkHelper.isNetworkConnected()).thenReturn(false)
+            // Suppose that local data store is empty
+            `when`(localDataSource.getMovies()).thenReturn(Resource.success(listOf()))
+            val movieResource = repository.getMovies()
+            // Verify that localDataSource.getMovies() is called
+            verify(localDataSource).getMovies()
+            // Verity that the status is error
+            assertThat(movieResource.status).isEqualTo(Status.ERROR)
+        }
     /**
      * -------------------- End of get movies test ------------------------------
-     */
-
-    /**
-     * -------------------- Get tv shows test ------------------------------
-     */
-    @Test
-    fun `get tv shows, returns not null`() {
-        val tvShows = repository.getTvShows()
-        Truth.assertThat(tvShows).isNotNull()
-    }
-
-    @Test
-    fun `get tv shows, return the list of tv show`() {
-        val tvShows = repository.getTvShows()
-        Truth.assertThat(tvShows).isNotEmpty()
-    }
-
-    @Test
-    fun `tv shows, return the same size with the actual dummy data`() {
-        val tvShows = repository.getTvShows()
-        Truth.assertThat(tvShows.size).isEqualTo(actualTvShowsSize)
-    }
-
-    @Test
-    fun `get tv shows, returns the same elements with the actual dummy data`() {
-        val tvShows = repository.getTvShows()
-        Truth.assertThat(tvShows).isEqualTo(actualTvShows)
-    }
-
-    /**
-     * -------------------- End of get tv shows test ------------------------------
-     */
-
-    /**
-     * -------------------- Get movie by id test ------------------------------
-     */
-    @Test
-    fun `get movie with not exist id, returns null`() {
-        val movie = repository.getMovieById(notExistMovieId)
-        Truth.assertThat(movie).isNull()
-    }
-
-    @Test
-    fun `get movie with valid id, returns a valid movie`() {
-        val movie = repository.getMovieById(randomMovieId)
-        Truth.assertThat(movie).isEqualTo(actualDummyMovie)
-    }
-
-    /**
-     * -------------------- End of get movie by id test ------------------------------
-     */
-
-    /**
-     * -------------------- Get tv show by id test ------------------------------
-     */
-    @Test
-    fun `get tv show with not exist id, returns null`() {
-        val tvShow = repository.getTvShowById(notExistTvShowId)
-        Truth.assertThat(tvShow).isNull()
-    }
-
-    @Test
-    fun `get tv show with valid id, returns a valid tv show`() {
-        val tvShow = repository.getTvShowById(randomTvShowId)
-        Truth.assertThat(tvShow).isEqualTo(actualDummyTvShow)
-    }
-
-    /**
-     * -------------------- End of get tv show by id test ------------------------------
      */
 }

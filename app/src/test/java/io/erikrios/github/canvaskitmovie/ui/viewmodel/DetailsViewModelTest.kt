6a29614958803160dc2ecb918dab20d7758dae2f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-04 15:32:02 +0700

    Add some test methods to get movie and tv show

--- app/src/test/java/io/erikrios/github/canvaskitmovie/ui/viewmodel/DetailsViewModelTest.kt
+++ app/src/test/java/io/erikrios/github/canvaskitmovie/ui/viewmodel/DetailsViewModelTest.kt
@@ -8,10 +8,18 @@ import io.erikrios.github.canvaskitmovie.data.model.TvShow
 import io.erikrios.github.canvaskitmovie.data.repository.CinemaRepositoryImpl
 import io.erikrios.github.canvaskitmovie.getOrAwaitValueTest
 import io.erikrios.github.canvaskitmovie.utils.DummyData
+import io.erikrios.github.canvaskitmovie.utils.Resource
+import io.erikrios.github.canvaskitmovie.utils.Status
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.verify
+import java.util.concurrent.TimeUnit
 
 @ExperimentalCoroutinesApi
 class DetailsViewModelTest {
@@ -19,11 +27,11 @@ class DetailsViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    /**
-     * We use this rule for the next submission, when the Coroutine is needed
-     */
     @get:Rule
     val mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    private lateinit var repository: CinemaRepositoryImpl
 
     private lateinit var detailsViewModel: DetailsViewModel
     private lateinit var actualMovies: List<Movie>
@@ -39,7 +47,8 @@ class DetailsViewModelTest {
 
     @Before
     fun setUp() {
-        detailsViewModel = DetailsViewModel(CinemaRepositoryImpl())
+        MockitoAnnotations.initMocks(this)
+
         actualMovies = DummyData.generateMovies()
         actualTvShows = DummyData.generateTvShows()
         actualMoviesSize = actualMovies.size
@@ -48,23 +57,68 @@ class DetailsViewModelTest {
         randomTvShowId = actualTvShows.random().id
         actualDummyMovie = DummyData.getMovieById(randomMovieId)
         actualDummyTvShow = DummyData.getTvShowById(randomTvShowId)
+
+        runBlockingTest {
+            Mockito.`when`(repository.getMovies()).thenReturn(Resource.success(actualMovies))
+            Mockito.`when`(repository.getTvShows()).thenReturn(Resource.success(actualTvShows))
+            Mockito.`when`(repository.getMovieById(randomMovieId)).thenReturn(
+                Resource.success(
+                    actualDummyMovie
+                )
+            )
+            Mockito.`when`(repository.getTvShowById(randomTvShowId)).thenReturn(
+                Resource.success(
+                    actualDummyTvShow
+                )
+            )
+            Mockito.`when`(repository.getMovieById(notExistMovieId)).thenReturn(
+                Resource.error(
+                    "Internal server error.", null
+                )
+            )
+            Mockito.`when`(repository.getTvShowById(notExistTvShowId)).thenReturn(
+                Resource.error(
+                    "Internal server error.", null
+                )
+            )
+        }
+
+        detailsViewModel = DetailsViewModel(repository)
     }
 
     /**
      * -------------------- Get movie by id test ------------------------------
      */
     @Test
-    fun `get movie with not exist id, returns null`() {
+    fun `get movie with not exist id, returns error status`() = runBlockingTest {
+        var movieResource =
+            detailsViewModel.movieState.getOrAwaitValueTest(100, TimeUnit.MILLISECONDS)
+        // Verify that the initial status is loading
+        assertThat(movieResource.status).isEqualTo(Status.LOADING)
         detailsViewModel.getMovieById(notExistMovieId)
-        val movie = detailsViewModel.movieState.getOrAwaitValueTest()
-        assertThat(movie).isNull()
+        // Verify that repository.getMovieById() is called
+        verify(repository).getMovieById(notExistMovieId)
+        // Verify that the status is error
+        movieResource = detailsViewModel.movieState.getOrAwaitValueTest()
+        assertThat(movieResource.status).isEqualTo(Status.ERROR)
+        // Verify that the movie data is null
+        assertThat(movieResource.data).isNull()
     }
 
     @Test
-    fun `get movie with valid id, returns a valid movie`() {
+    fun `get movie with valid id, returns a valid movie`() = runBlockingTest {
+        var movieResource =
+            detailsViewModel.movieState.getOrAwaitValueTest(100, TimeUnit.MILLISECONDS)
+        // Verify that the initial status is loading
+        assertThat(movieResource.status).isEqualTo(Status.LOADING)
         detailsViewModel.getMovieById(randomMovieId)
-        val movie = detailsViewModel.movieState.getOrAwaitValueTest()
-        assertThat(movie).isEqualTo(actualDummyMovie)
+        // Verify that repository.getMovieById() is called
+        verify(repository).getMovieById(randomMovieId)
+        // Verify that the status is success
+        movieResource = detailsViewModel.movieState.getOrAwaitValueTest()
+        assertThat(movieResource.status).isEqualTo(Status.SUCCESS)
+        // Verify that the movie data is equals to actual dummy movie
+        assertThat(movieResource.data).isEqualTo(actualDummyMovie)
     }
 
     /**
@@ -75,17 +129,35 @@ class DetailsViewModelTest {
      * -------------------- Get tv show by id test ------------------------------
      */
     @Test
-    fun `get tv show with not exist id, returns null`() {
+    fun `get tv show with not exist id, returns null`() = runBlockingTest {
+        var tvShowResource =
+            detailsViewModel.tvShowState.getOrAwaitValueTest(100, TimeUnit.MILLISECONDS)
+        // Verify that the initial status is loading
+        assertThat(tvShowResource.status).isEqualTo(Status.LOADING)
         detailsViewModel.getTvShowById(notExistTvShowId)
-        val tvShow = detailsViewModel.tvShowState.getOrAwaitValueTest()
-        assertThat(tvShow).isNull()
+        // Verify that repository.getTvShowById() is called
+        verify(repository).getTvShowById(notExistTvShowId)
+        // Verify that the status is error
+        tvShowResource = detailsViewModel.tvShowState.getOrAwaitValueTest()
+        assertThat(tvShowResource.status).isEqualTo(Status.ERROR)
+        // Verify that the tv show data is null
+        assertThat(tvShowResource.data).isNull()
     }
 
     @Test
-    fun `get tv show with valid id, returns a valid tv show`() {
+    fun `get tv show with valid id, returns a valid tv show`() = runBlockingTest {
+        var tvShowResource =
+            detailsViewModel.tvShowState.getOrAwaitValueTest(100, TimeUnit.MILLISECONDS)
+        // Verify that the initial status is loading
+        assertThat(tvShowResource.status).isEqualTo(Status.LOADING)
         detailsViewModel.getTvShowById(randomTvShowId)
-        val tvShow = detailsViewModel.tvShowState.getOrAwaitValueTest()
-        assertThat(tvShow).isEqualTo(actualDummyTvShow)
+        // Verify that repository.getTvShowById() is called
+        verify(repository).getTvShowById(randomTvShowId)
+        // Verify that the status is success
+        tvShowResource = detailsViewModel.tvShowState.getOrAwaitValueTest()
+        assertThat(tvShowResource.status).isEqualTo(Status.SUCCESS)
+        // Verify that the tv show data is equals to actual dummy tv show
+        assertThat(tvShowResource.data).isEqualTo(actualDummyTvShow)
     }
 
     /**

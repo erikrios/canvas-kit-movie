7825142605d70898d4c324a98aa5900f6b98cb22
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-04 11:26:45 +0700

    Create FakeCinemaRepositoryImpl.kt

---
+++ app/src/test/java/io/erikrios/github/canvaskitmovie/data/repository/FakeCinemaRepositoryImpl.kt
@@ -0,0 +1,83 @@
+package io.erikrios.github.canvaskitmovie.data.repository
+
+import io.erikrios.github.canvaskitmovie.data.model.Movie
+import io.erikrios.github.canvaskitmovie.data.model.TvShow
+import io.erikrios.github.canvaskitmovie.data.source.DataSource
+import io.erikrios.github.canvaskitmovie.data.source.locale.LocaleDataSource
+import io.erikrios.github.canvaskitmovie.utils.NetworkHelper
+import io.erikrios.github.canvaskitmovie.utils.Resource
+
+class FakeCinemaRepositoryImpl(
+    private val networkHelper: NetworkHelper,
+    private val localeDataSource: DataSource,
+    private val remoteDataSource: DataSource
+) : CinemaRepository {
+
+    override suspend fun getMovies(): Resource<List<Movie>> {
+        if (!networkHelper.isNetworkConnected()) {
+            val cachedMovies = localeDataSource.getMovies()
+            cachedMovies.data?.let {
+                return if (it.isEmpty())
+                    Resource.error(
+                        "Couldn't reach the server. Check your internet connection",
+                        null
+                    )
+                else
+                    cachedMovies
+            } ?: return Resource.error(
+                "Couldn't reach the server. Check your internet connection",
+                null
+            )
+        } else {
+            val moviesResources = remoteDataSource.getMovies()
+            moviesResources.data?.let { (localeDataSource as LocaleDataSource).addCaches(it) }
+            return moviesResources
+        }
+    }
+
+    override suspend fun getTvShows(): Resource<List<TvShow>> {
+        if (!networkHelper.isNetworkConnected()) {
+            val cachedTvShows = localeDataSource.getTvShows()
+            cachedTvShows.data?.let {
+                return if (it.isEmpty())
+                    Resource.error(
+                        "Couldn't reach the server. Check your internet connection",
+                        null
+                    )
+                else
+                    cachedTvShows
+            } ?: return Resource.error(
+                "Couldn't reach the server. Check your internet connection",
+                null
+            )
+        } else {
+            val tvShowsResources = remoteDataSource.getTvShows()
+            tvShowsResources.data?.let { (localeDataSource as LocaleDataSource).addCaches(it) }
+            return tvShowsResources
+        }
+    }
+
+    override suspend fun getMovieById(id: Int): Resource<Movie> {
+        if (!networkHelper.isNetworkConnected()) {
+            val cachedMovie = localeDataSource.getMovieDetails(id)
+            cachedMovie.data?.let { return cachedMovie } ?: return Resource.error(
+                "Couldn't reach the server. Check your internet connection",
+                null
+            )
+        } else {
+            return remoteDataSource.getMovieDetails(id)
+        }
+    }
+
+    override suspend fun getTvShowById(id: Int): Resource<TvShow> {
+        if (!networkHelper.isNetworkConnected()) {
+            val cachedTvShow = localeDataSource.getTvShowDetails(id)
+            cachedTvShow.data?.let { return cachedTvShow } ?: return Resource.error(
+                "Couldn't reach the server. Check your internet connection",
+                null
+            )
+        } else {
+            return remoteDataSource.getTvShowDetails(id)
+        }
+    }
+}

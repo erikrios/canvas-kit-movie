e85db1271629d3581941cbe033acde87e14b8094
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-06 10:49:59 +0700

    Implements the Espresso Idling Resource

--- app/src/main/java/io/erikrios/github/canvaskitmovie/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/data/source/remote/RemoteDataSource.kt
@@ -4,6 +4,8 @@ import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.data.model.TvShow
 import io.erikrios.github.canvaskitmovie.data.source.DataSource
 import io.erikrios.github.canvaskitmovie.network.TheMovieDatabaseApiHelper
+import io.erikrios.github.canvaskitmovie.utils.EspressoIdlingResource.decrement
+import io.erikrios.github.canvaskitmovie.utils.EspressoIdlingResource.increment
 import io.erikrios.github.canvaskitmovie.utils.Resource
 import javax.inject.Inject
 
@@ -11,46 +13,68 @@ class RemoteDataSource @Inject constructor(private val apiHelper: TheMovieDataba
     DataSource {
     override suspend fun getMovies(): Resource<List<Movie>> {
         return try {
+            increment()
             val response = apiHelper.getMovies()
-            if (response.isSuccessful)
+            if (response.isSuccessful) {
+                decrement()
                 Resource.success(response.body()?.results)
-            else
+            } else {
+                decrement()
                 Resource.error("Internal server error.", null)
+            }
         } catch (e: Exception) {
+            decrement()
             Resource.error("Couldn't reach the server. Check your internet connection", null)
         }
     }
 
     override suspend fun getTvShows(): Resource<List<TvShow>> {
         return try {
+            increment()
             val response = apiHelper.getTvShows()
-            if (response.isSuccessful)
+            if (response.isSuccessful) {
+                decrement()
                 Resource.success(response.body()?.results)
-            else
+            } else {
+                decrement()
                 Resource.error("Internal server error.", null)
+            }
         } catch (e: Exception) {
+            decrement()
             Resource.error("Couldn't reach the server. Check your internet connection", null)
         }
     }
 
     override suspend fun getMovieDetails(id: Int): Resource<Movie> {
         return try {
+            increment()
             val response = apiHelper.getMovieDetails(id)
-            if (response.isSuccessful)
+            if (response.isSuccessful) {
+                decrement()
                 Resource.success(response.body())
-            else Resource.error("Internal server error.", null)
+            } else {
+                decrement()
+                Resource.error("Internal server error.", null)
+            }
         } catch (e: Exception) {
+            decrement()
             Resource.error("Couldn't reach the server. Check your internet connection", null)
         }
     }
 
     override suspend fun getTvShowDetails(id: Int): Resource<TvShow> {
         return try {
+            increment()
             val response = apiHelper.getTvShowDetails(id)
-            if (response.isSuccessful)
+            if (response.isSuccessful) {
+                decrement()
                 Resource.success(response.body())
-            else Resource.error("Internal server error.", null)
+            } else {
+                decrement()
+                Resource.error("Internal server error.", null)
+            }
         } catch (e: Exception) {
+            decrement()
             Resource.error("Couldn't reach the server. Check your internet connection", null)
         }
     }

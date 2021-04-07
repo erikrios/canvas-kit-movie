0342fea5292368febc83f2d642163df54152bef9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-07 21:02:25 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/favorites/FavoriteMoviesFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/favorites/FavoriteMoviesFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import io.erikrios.github.canvaskitmovie.databinding.FragmentTvShowsBinding
+import io.erikrios.github.canvaskitmovie.databinding.FragmentFavoriteMoviesBinding
 
 class FavoriteMoviesFragment : Fragment() {
 
-    private var _binding: FragmentTvShowsBinding? = null
+    private var _binding: FragmentFavoriteMoviesBinding? = null
     private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentTvShowsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteMoviesBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/favorites/FavoriteTvShowsFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/favorites/FavoriteTvShowsFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import io.erikrios.github.canvaskitmovie.databinding.FragmentFavoriteMoviesBinding
+import io.erikrios.github.canvaskitmovie.databinding.FragmentFavoriteTvShowsBinding
 
 class FavoriteTvShowsFragment : Fragment() {
 
-    private var _binding: FragmentFavoriteMoviesBinding? = null
+    private var _binding: FragmentFavoriteTvShowsBinding? = null
     private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFavoriteMoviesBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteTvShowsBinding.inflate(inflater, container, false)
         return binding?.root
     }
 

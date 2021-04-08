642a4aee7fd0f09158e902b4c78d2e01a2c8fb9d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-08 21:24:49 +0700

    Add some methods to handle toolbar, adapater, and recycler view

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/favorites/FavoriteTvShowsFragment.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/view/favorites/FavoriteTvShowsFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import io.erikrios.github.canvaskitmovie.R
+import io.erikrios.github.canvaskitmovie.data.model.TvShow
 import io.erikrios.github.canvaskitmovie.databinding.FragmentFavoriteTvShowsBinding
+import io.erikrios.github.canvaskitmovie.ui.adapter.FavoriteCinemaAdapter
+import io.erikrios.github.canvaskitmovie.utils.DummyData
 
 class FavoriteTvShowsFragment : Fragment() {
 
     private var _binding: FragmentFavoriteTvShowsBinding? = null
     private val binding get() = _binding
+    private lateinit var adapter: FavoriteCinemaAdapter<TvShow>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +28,34 @@ class FavoriteTvShowsFragment : Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleToolbar()
+        handleAdapter(DummyData.generateTvShows())
+        handleRecyclerView()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun handleToolbar() {
+        binding?.toolbar?.apply {
+            navigationIcon =
+                ContextCompat.getDrawable(context, R.drawable.ic_baseline_arrow_back_24)
+            setNavigationOnClickListener { findNavController().popBackStack() }
+        }
+    }
+
+    private fun handleAdapter(tvShows: List<TvShow>) {
+        adapter = FavoriteCinemaAdapter {
+            Toast.makeText(context, it.name, Toast.LENGTH_SHORT).show()
+        }
+        adapter.setCinemas(tvShows)
+    }
+
+    private fun handleRecyclerView() {
+        binding?.rvFavoriteTvShows?.adapter = adapter
     }
 }

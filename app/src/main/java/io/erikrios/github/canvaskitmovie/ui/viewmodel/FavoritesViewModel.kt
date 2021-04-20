d58c4034a65d8ee85ae90a354091e823dd585a83
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 21:39:58 +0700

    Implements the paging

--- app/src/main/java/io/erikrios/github/canvaskitmovie/ui/viewmodel/FavoritesViewModel.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/ui/viewmodel/FavoritesViewModel.kt
@@ -1,46 +1,24 @@
 package io.erikrios.github.canvaskitmovie.ui.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.erikrios.github.canvaskitmovie.data.model.Movie
 import io.erikrios.github.canvaskitmovie.data.model.TvShow
 import io.erikrios.github.canvaskitmovie.data.repository.CinemaRepository
-import io.erikrios.github.canvaskitmovie.utils.Resource
 import io.erikrios.github.canvaskitmovie.utils.SortUtils
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoritesViewModel @Inject constructor(private val repository: CinemaRepository) :
     ViewModel() {
 
-    private val _favoriteMoviesState = MutableLiveData<Resource<List<Movie>>>().apply {
-        value = Resource.loading(null)
-    }
+    fun getFavoriteMovies(sort: SortUtils.Sort): LiveData<PagedList<Movie>> =
+        LivePagedListBuilder(repository.getFavoriteMovies(sort), 20).build()
 
-    val favoriteMoviesState: LiveData<Resource<List<Movie>>> get() = _favoriteMoviesState
 
-    private val _favoriteTvShowsState = MutableLiveData<Resource<List<TvShow>>>().apply {
-        value = Resource.loading(null)
-    }
-
-    val favoriteTvShowsState: LiveData<Resource<List<TvShow>>> get() = _favoriteTvShowsState
-
-    fun getFavoriteMovies(sort: SortUtils.Sort): Job {
-        return viewModelScope.launch {
-            val movies = repository.getFavoriteMovies(sort)
-            _favoriteMoviesState.value = Resource.success(movies)
-        }
-    }
-
-    fun getFavoriteTvShows(sort: SortUtils.Sort): Job {
-        return viewModelScope.launch {
-            val tvShows = repository.getFavoriteTvShows(sort)
-            _favoriteTvShowsState.value = Resource.success(tvShows)
-        }
-    }
+    fun getFavoriteTvShows(sort: SortUtils.Sort): LiveData<PagedList<TvShow>> =
+        LivePagedListBuilder(repository.getFavoriteTvShows(sort), 20).build()
 }

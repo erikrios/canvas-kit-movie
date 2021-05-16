81f6959cb88cc9e702190ba796a5e0ce6ac92266
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-16 21:26:20 +0700

    Remove unnecessary code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/di/AppModule.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/di/AppModule.kt
@@ -3,9 +3,6 @@ package io.erikrios.github.canvaskitmovie.di
 import io.erikrios.github.canvaskitmovie.details.MovieDetailsViewModel
 import io.erikrios.github.canvaskitmovie.details.TrendingDetailsViewModel
 import io.erikrios.github.canvaskitmovie.details.TvShowDetailsViewModel
-import io.erikrios.github.canvaskitmovie.favorites.FavoriteMoviesViewModel
-import io.erikrios.github.canvaskitmovie.favorites.FavoriteTrendingsViewModel
-import io.erikrios.github.canvaskitmovie.favorites.FavoriteTvShowsViewModel
 import io.erikrios.github.canvaskitmovie.movies.DiscoverMoviesViewModel
 import io.erikrios.github.canvaskitmovie.trending.DiscoverTrendingViewModel
 import io.erikrios.github.canvaskitmovie.tvshows.DiscoverTvShowsViewModel
@@ -26,7 +23,4 @@ val viewModelModule = module {
     viewModel { MovieDetailsViewModel(get()) }
     viewModel { TvShowDetailsViewModel(get()) }
     viewModel { TrendingDetailsViewModel(get()) }
-    viewModel { FavoriteMoviesViewModel(get()) }
-    viewModel { FavoriteTvShowsViewModel(get()) }
-    viewModel { FavoriteTrendingsViewModel(get()) }
 }

b346d64bf39fdd8eed0406d37e53cc35e165b7fc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-16 13:01:00 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/MainActivity.kt
@@ -1,27 +1,17 @@
 package io.erikrios.github.canvaskitmovie
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
 import io.erikrios.github.canvaskitmovie.databinding.ActivityMainBinding
-import io.erikrios.github.canvaskitmovie.main.MainViewModel
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.Theme_CanvasKitMovie_NoActionBar)
         setContentView(binding.root)
-        mainViewModel.apply {
-            getMovies()
-            getTvShows()
-            getTrending()
-        }
     }
 }

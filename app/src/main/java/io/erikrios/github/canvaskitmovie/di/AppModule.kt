8e9c989764b16b22a7ec68764fa7585b078eb759
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-16 16:31:08 +0700

    Add FavoriteTvShowsViewModel

--- app/src/main/java/io/erikrios/github/canvaskitmovie/di/AppModule.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/di/AppModule.kt
@@ -5,6 +5,7 @@ import io.erikrios.github.canvaskitmovie.details.MovieDetailsViewModel
 import io.erikrios.github.canvaskitmovie.details.TrendingDetailsViewModel
 import io.erikrios.github.canvaskitmovie.details.TvShowDetailsViewModel
 import io.erikrios.github.canvaskitmovie.favorites.FavoriteMoviesViewModel
+import io.erikrios.github.canvaskitmovie.favorites.FavoriteTvShowsViewModel
 import io.erikrios.github.canvaskitmovie.movies.DiscoverMoviesViewModel
 import io.erikrios.github.canvaskitmovie.trending.DiscoverTrendingViewModel
 import io.erikrios.github.canvaskitmovie.tvshows.DiscoverTvShowsViewModel
@@ -25,4 +26,5 @@ val viewModelModule = module {
     viewModel { TvShowDetailsViewModel(get()) }
     viewModel { TrendingDetailsViewModel(get()) }
     viewModel { FavoriteMoviesViewModel(get()) }
+    viewModel { FavoriteTvShowsViewModel(get()) }
 }

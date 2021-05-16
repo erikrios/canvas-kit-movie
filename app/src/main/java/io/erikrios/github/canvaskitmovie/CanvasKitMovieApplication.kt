da54fdc72152389a31e4addc3cb61cfc7f5c1453
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-16 11:50:24 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/canvaskitmovie/CanvasKitMovieApplication.kt
+++ app/src/main/java/io/erikrios/github/canvaskitmovie/CanvasKitMovieApplication.kt
@@ -1,7 +1,27 @@
 package io.erikrios.github.canvaskitmovie
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import io.erikrios.github.canvaskitmovie.core.di.databaseModule
+import io.erikrios.github.canvaskitmovie.core.di.networkModule
+import io.erikrios.github.canvaskitmovie.core.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-@HiltAndroidApp
-class CanvasKitMovieApplication : Application()
+class CanvasKitMovieApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@CanvasKitMovieApplication)
+            modules(
+                listOf(
+                    databaseModule,
+                    networkModule,
+                    repositoryModule
+                )
+            )
+        }
+    }
+}

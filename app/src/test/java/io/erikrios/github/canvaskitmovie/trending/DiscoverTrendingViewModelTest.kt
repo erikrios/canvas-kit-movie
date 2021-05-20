43c71b52584424a3ff644ecc12ce26f3b977cbe3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-20 07:42:55 +0700

    Create DiscoverTrendingViewModelTest.kt

---
+++ app/src/test/java/io/erikrios/github/canvaskitmovie/trending/DiscoverTrendingViewModelTest.kt
@@ -0,0 +1,58 @@
+package io.erikrios.github.canvaskitmovie.trending
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import io.erikrios.github.canvaskitmovie.MainCoroutineRule
+import io.erikrios.github.canvaskitmovie.getOrAwaitValueTest
+import io.github.erikrios.canvaskitmovie.core.data.Resource
+import io.github.erikrios.canvaskitmovie.core.domain.model.Trending
+import io.github.erikrios.canvaskitmovie.core.domain.usecase.TrendingUseCase
+import io.github.erikrios.canvaskitmovie.core.utils.DummyData.generateTrendings
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+@ExperimentalCoroutinesApi
+class DiscoverTrendingViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    private lateinit var trendingUseCase: TrendingUseCase
+
+    private lateinit var discoverTrendingViewModel: DiscoverTrendingViewModel
+    private lateinit var actualTrendings: List<Trending>
+
+    @Suppress("DEPRECATION")
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        actualTrendings = generateTrendings()
+        `when`(trendingUseCase.getTrendings()).thenReturn(flow {
+            emit(
+                Resource.Success(
+                    actualTrendings
+                )
+            )
+        })
+        discoverTrendingViewModel = DiscoverTrendingViewModel(trendingUseCase)
+    }
+
+    @Test
+    fun `get trendings return the list of trendings`() {
+        val trendingsResource = discoverTrendingViewModel.trendingsState.getOrAwaitValueTest()
+        assertThat(trendingsResource).isInstanceOf(Resource.Success::class.java)
+        val trendings = trendingsResource.data
+        assertThat(trendings).isNotEmpty()
+    }
+}

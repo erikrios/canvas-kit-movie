db3141c334b5fd4cf9b39da84ff16c560959ac8a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-20 07:39:08 +0700

    Add a method to generate trending

--- core/src/main/java/io/github/erikrios/canvaskitmovie/core/utils/DummyData.kt
+++ core/src/main/java/io/github/erikrios/canvaskitmovie/core/utils/DummyData.kt
@@ -1,9 +1,6 @@
 package io.github.erikrios.canvaskitmovie.core.utils
 
-import io.github.erikrios.canvaskitmovie.core.domain.model.Creator
-import io.github.erikrios.canvaskitmovie.core.domain.model.Genre
-import io.github.erikrios.canvaskitmovie.core.domain.model.Movie
-import io.github.erikrios.canvaskitmovie.core.domain.model.TvShow
+import io.github.erikrios.canvaskitmovie.core.domain.model.*
 
 object DummyData {
 
@@ -884,7 +881,453 @@ object DummyData {
         )
     }
 
+    fun generateTrendings(): List<Trending> {
+        return mutableListOf(
+            Trending(
+                791373,
+                false,
+                "/pcDc2WJAYGJTTvRSEIpRZwM3Ola.jpg",
+                70000000,
+                listOf(
+                    Genre(28, "Action"),
+                    Genre(12, "Adventure"),
+                    Genre(14, "Fantasy"),
+                    Genre(878, "Science Fiction")
+                ),
+                "en",
+                "Zack Snyder's Justice League",
+                "Zack Snyder's Justice League",
+                "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.",
+                11783.352,
+                "/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg",
+                "2021-03-18",
+                0,
+                "Released",
+                8.7,
+                3389, false
+            ),
+            Trending(
+                527774,
+                false,
+                "/hJuDvwzS0SPlsE6MNFOpznQltDZ.jpg",
+                0,
+                listOf(
+                    Genre(16, "Animation"),
+                    Genre(12, "Adventure"),
+                    Genre(14, "Fantasy"),
+                    Genre(10751, "Family"),
+                    Genre(28, "Action")
+                ),
+                "en",
+                "Raya and the Last Dragon",
+                "Raya and the Last Dragon",
+                "Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. But when an evil force threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, that same evil has returned and it’s up to a lone warrior, Raya, to track down the legendary last dragon to restore the fractured land and its divided people.",
+                3067.315,
+                "/lPsD10PP4rgUGiGR4CCXA6iY0QQ.jpg",
+                "2021-03-03",
+                56482606,
+                "Released",
+                8.4,
+                1628, false
+            ),
+            Trending(
+                581389,
+                false,
+                "/drulhSX7P5TQlEMQZ3JoXKSDEfz.jpg",
+                21000000,
+                listOf(
+                    Genre(18, "Drama"),
+                    Genre(14, "Fantasy"),
+                    Genre(878, "Science Fiction")
+                ),
+                "ko",
+                "승리호",
+                "Space Sweepers",
+                "When the crew of a space junk collector ship called The Victory discovers a humanoid robot named Dorothy that's known to be a weapon of mass destruction, they get involved in a risky business deal which puts their lives at stake.",
+                3507.528,
+                "/lykPQ7lgrLJPwLlSyetVXsl2wDf.jpg",
+                "2021-02-05",
+                0,
+                "Released",
+                7.2,
+                417, false
+            ),
+            Trending(
+                793723,
+                false,
+                "/gzJnMEMkHowkUndn9gCr8ghQPzN.jpg",
+                0,
+                listOf(
+                    Genre(53, "Thriller"),
+                    Genre(28, "Action"),
+                    Genre(18, "Drama")
+                ),
+                "fr",
+                "Sentinelle",
+                "Sentinelle",
+                "Transferred home after a traumatizing combat mission, a highly trained French soldier uses her lethal skills to hunt down the man who hurt her sister.",
+                2266.597,
+                "/fFRq98cW9lTo6di2o4lK1qUAWaN.jpg",
+                "2021-03-05",
+                0,
+                "Released",
+                6.0,
+                264, false
+            ),
+            Trending(
+                587807,
+                false,
+                "/fev8UFNFFYsD5q7AcYS8LyTzqwl.jpg",
+                50000000,
+                listOf(
+                    Genre(28, "Action"),
+                    Genre(35, "Comedy"),
+                    Genre(10751, "Family"),
+                    Genre(16, "Animation")
+                ),
+                "en",
+                "Tom & Jerry",
+                "Tom & Jerry",
+                "Tom the cat and Jerry the mouse get kicked out of their home and relocate to a fancy New York hotel, where a scrappy employee named Kayla will lose her job if she can’t evict Jerry before a high-class wedding at the hotel. Her solution? Hiring Tom to get rid of the pesky mouse.",
+                1944.478,
+                "/6KErczPBROQty7QoIsaa6wJYXZi.jpg",
+                "2021-02-11",
+                66890000,
+                "Released",
+                7.4,
+                962, false
+            ),
+            Trending(
+                458576,
+                false,
+                "/8tNX8s3j1O0eqilOQkuroRLyOZA.jpg",
+                60000000,
+                listOf(
+                    Genre(14, "Fantasy"),
+                    Genre(28, "Action"),
+                    Genre(12, "Adventure")
+                ),
+                "en",
+                "Monster Hunter",
+                "Monster Hunter",
+                "A portal transports Cpt. Artemis and an elite unit of soldiers to a strange world where powerful monsters rule with deadly ferocity. Faced with relentless danger, the team encounters a mysterious hunter who may be their only hope to find a way home.",
+                1900.233,
+                "/1UCOF11QCw8kcqvce8LKOO6pimh.jpg",
+                "2020-12-03",
+                25814306,
+                "Released",
+                7.2,
+                1229, false
+            ),
+            Trending(
+                535313,
+                false,
+                "/iopYFB1b6Bh7FWZh3onQhph1sih.jpg",
+                200000000,
+                listOf(
+                    Genre(28, "Action"),
+                    Genre(878, "Science Fiction")
+                ),
+                "en",
+                "Godzilla vs. Kong",
+                "Godzilla vs. Kong",
+                "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+                2675.469,
+                "/asmxTLzVSLTkIwybSB0DJHDKw8o.jpg",
+                "2021-03-24",
+                0,
+                "Released",
+                7.6,
+                35, false
+            ),
+            Trending(
+                468552,
+                false,
+                "/egg7KFi18TSQc1s24RMmR9i2zO6.jpg",
+                200000000,
+                listOf(
+                    Genre(14, "Fantasy"),
+                    Genre(28, "Action"),
+                    Genre(12, "Adventure")
+                ),
+                "en",
+                "Wonder Woman 1984",
+                "Wonder Woman 1984",
+                "A botched store robbery places Wonder Woman in a global battle against a powerful and mysterious ancient force that puts her powers in jeopardy.",
+                1693.774,
+                "/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+                "2020-12-16",
+                165160000,
+                "Released",
+                6.8,
+                4446, false
+            ),
+            Trending(
+                587996,
+                false,
+                "/6TPZSJ06OEXeelx1U1VIAt0j9Ry.jpg",
+                0,
+                listOf(
+                    Genre(28, "Action"),
+                    Genre(80, "Crime"),
+                    Genre(53, "Thriller")
+                ),
+                "es",
+                "Bajocero",
+                "Below Zero",
+                "When a prisoner transfer van is attacked, the cop in charge must fight those inside and outside while dealing with a silent foe: the icy temperatures.",
+                1644.589,
+                "/dWSnsAGTfc8U27bWsy2RfwZs0Bs.jpg",
+                "2021-01-29",
+                0,
+                "Released",
+                6.4,
+                468, false
+            ),
+            Trending(
+                544401,
+                false,
+                "/uQtqiAu2bBlokqjlURVLEha6zoi.jpg",
+                0,
+                listOf(
+                    Genre(80, "Crime"),
+                    Genre(18, "Drama")
+                ),
+                "en",
+                "Cherry",
+                "Cherry",
+                "Cherry drifts from college dropout to army medic in Iraq - anchored only by his true love, Emily. But after returning from the war with PTSD, his life spirals into drugs and crime as he struggles to find his place in the world.",
+                1629.653,
+                "/pwDvkDyaHEU9V7cApQhbcSJMG1w.jpg",
+                "2021-02-26",
+                0,
+                "Released",
+                7.7,
+                344, false
+            ),
+            Trending(
+                775996,
+                false,
+                "/lOSdUkGQmbAl5JQ3QoHqBZUbZhC.jpg",
+                0,
+                listOf(
+                    Genre(53, "Thriller"),
+                    Genre(28, "Action"),
+                    Genre(878, "Science Fiction")
+                ),
+                "en",
+                "Outside the Wire",
+                "Outside the Wire",
+                "In the near future, a drone pilot is sent into a deadly militarized zone and must work with an android officer to locate a doomsday device.",
+                1096.334,
+                "/6XYLiMxHAaCsoyrVo38LBWMw2p8.jpg",
+                "2021-01-15",
+                0,
+                "Released",
+                6.5,
+                894, false
+            ),
+            Trending(
+                484718,
+                false,
+                "/vKzbIoHhk1z9DWYi8kyFe9Gg0HF.jpg",
+                60000000,
+                listOf(
+                    Genre(35, "Comedy")
+                ),
+                "en",
+                "Coming 2 America",
+                "Coming 2 America",
+                "Prince Akeem Joffer is set to become King of Zamunda when he discovers he has a son he never knew about in America – a street savvy Queens native named Lavelle. Honoring his royal father's dying wish to groom this son as the crown prince, Akeem and Semmi set off to America once again.",
+                878.181,
+                "/nWBPLkqNApY5pgrJFMiI9joSI30.jpg",
+                "2021-03-05",
+                0,
+                "Released",
+                6.9,
+                1012, false
+            ),
+            Trending(
+                649087,
+                false,
+                "/7KL4yJ4JsbtS1BNRilUApLvMnc5.jpg",
+                0,
+                listOf(
+                    Genre(18, "Drama"),
+                    Genre(53, "Thriller")
+                ),
+                "sv",
+                "Red Dot",
+                "Red Dot",
+                "On a hiking trip to rekindle their marriage, a couple find themselves fleeing for their lives in the unforgiving wilderness from an unknown shooter.",
+                936.356,
+                "/xZ2KER2gOHbuHP2GJoODuXDSZCb.jpg",
+                "2021-02-11",
+                0,
+                "Released",
+                6.2,
+                365, false
+            ),
+            Trending(
+                651571,
+                false,
+                "/nz8xWrTKZzA5A7FgxaM4kfAoO1W.jpg",
+                0,
+                listOf(
+                    Genre(878, "Science Fiction"),
+                    Genre(28, "Action")
+                ),
+                "en",
+                "Breach",
+                "Breach",
+                "A hardened mechanic must stay awake and maintain an interstellar ark fleeing the dying planet Earth with a few thousand lucky souls on board... the last of humanity. Unfortunately, humans are not the only passengers. A shapeshifting alien creature has taken residence, its only goal is to kill as many people as possible. The crew must think quickly to stop this menace before it destroys mankind.",
+                917.923,
+                "/13B6onhL6FzSN2KaNeQeMML05pS.jpg",
+                "2020-12-17",
+                0,
+                "Released",
+                4.5,
+                323, false
+            ),
+            Trending(
+                602269,
+                false,
+                "/vfuzELmhBjBTswXj2Vqxnu5ge4g.jpg",
+                30000000,
+                listOf(
+                    Genre(53, "Thriller"),
+                    Genre(80, "Crime")
+                ),
+                "en",
+                "The Little Things",
+                "The Little Things",
+                "Deputy Sheriff Joe \\\"Deke\\\" Deacon joins forces with Sgt. Jim Baxter to search for a serial killer who's terrorizing Los Angeles. As they track the culprit, Baxter is unaware that the investigation is dredging up echoes of Deke's past, uncovering disturbing secrets that could threaten more than his case.",
+                885.01,
+                "/c7VlGCCgM9GZivKSzBgzuOVxQn7.jpg",
+                "2021-01-28",
+                23205000,
+                "Released",
+                6.4,
+                632, false
+            ),
+            Trending(
+                590706,
+                false,
+                "/jeAQdDX9nguP6YOX6QSWKDPkbBo.jpg",
+                23000000,
+                listOf(
+                    Genre(14, "Fantasy"),
+                    Genre(28, "Action"),
+                    Genre(878, "Science Fiction")
+                ),
+                "en",
+                "Jiu Jitsu",
+                "Jiu Jitsu",
+                "Every six years, an ancient order of jiu-jitsu fighters joins forces to battle a vicious race of alien invaders. But when a celebrated war hero goes down in defeat, the fate of the planet and mankind hangs in the balance.",
+                853.208,
+                "/eLT8Cu357VOwBVTitkmlDEg32Fs.jpg",
+                "2020-11-20",
+                99924,
+                "Released",
+                5.2,
+                340, false
+            ),
+            Trending(
+                522444,
+                false,
+                "/fRrpOILyXuWaWLmqF7kXeMVwITQ.jpg",
+                0,
+                listOf(
+                    Genre(27, "Horror"),
+                    Genre(53, "Thriller"),
+                    Genre(12, "Adventure"),
+                    Genre(9648, "Mystery")
+                ),
+                "en",
+                "Black Water: Abyss",
+                "Black Water: Abyss",
+                "An adventure-loving couple convince their friends to explore a remote, uncharted cave system in the forests of Northern Australia. With a tropical storm approaching, they abseil into the mouth of the cave, but when the caves start to flood, tensions rise as oxygen levels fall and the friends find themselves trapped. Unknown to them, the storm has also brought in a pack of dangerous and hungry crocodiles.",
+                858.945,
+                "/95S6PinQIvVe4uJAd82a2iGZ0rA.jpg",
+                "2020-07-09",
+                0,
+                "Released",
+                5.0,
+                170, false
+            ),
+            Trending(
+                755812,
+                false,
+                "/yR27bZPIkNhpGEIP3jKV2EifTgo.jpg",
+                0,
+                listOf(
+                    Genre(16, "Animation"),
+                    Genre(10751, "Family")
+                ),
+                "fr",
+                "Miraculous World: New York, United HeroeZ",
+                "Miraculous World: New York, United HeroeZ",
+                "During a school field trip, Ladybug and Cat Noir meet the American superheroes, whom they have to save from an akumatised super-villain. They discover that Miraculous exist in the United States too.",
+                882.522,
+                "/50AlM1eAqXHEG8Z5lzrTbWyiew9.jpg",
+                "2020-09-26",
+                0,
+                "Released",
+                8.3,
+                529, false
+            ),
+            Trending(
+                664767,
+                false,
+                "/9xeEGUZjgiKlI69jwIOi0hjKUIk.jpg",
+                0,
+                listOf(
+                    Genre(14, "Fantasy"),
+                    Genre(28, "Action"),
+                    Genre(12, "Adventure"),
+                    Genre(16, "Animation")
+                ),
+                "en",
+                "Mortal Kombat Legends: Scorpion's Revenge",
+                "Mortal Kombat Legends: Scorpion's Revenge",
+                "After the vicious slaughter of his family by stone-cold mercenary Sub-Zero, Hanzo Hasashi is exiled to the torturous Netherrealm. There, in exchange for his servitude to the sinister Quan Chi, he’s given a chance to avenge his family – and is resurrected as Scorpion, a lost soul bent on revenge. Back on Earthrealm, Lord Raiden gathers a team of elite warriors – Shaolin monk Liu Kang, Special Forces officer Sonya Blade and action star Johnny Cage – an unlikely band of heroes with one chance to save humanity. To do this, they must defeat Shang Tsung’s horde of Outworld gladiators and reign over the Mortal Kombat tournament.",
+                798.951,
+                "/4VlXER3FImHeFuUjBShFamhIp9M.jpg",
+                "2020-04-12",
+                0,
+                "Released",
+                8.4,
+                805, false
+            ),
+            Trending(
+                529203,
+                false,
+                "/cjaOSjsjV6cl3uXdJqimktT880L.jpg",
+                65000000,
+                listOf(
+                    Genre(10751, "Family"),
+                    Genre(14, "Fantasy"),
+                    Genre(16, "Animation"),
+                    Genre(35, "Comedy")
+                ),
+                "en",
+                "The Croods: A New Age",
+                "The Croods: A New Age",
+                "Searching for a safer habitat, the prehistoric Crood family discovers an idyllic, walled-in paradise that meets all of its needs. Unfortunately, they must also learn to live with the Bettermans -- a family that's a couple of steps above the Croods on the evolutionary ladder. As tensions between the new neighbors start to rise, a new threat soon propels both clans on an epic adventure that forces them to embrace their differences, draw strength from one another, and survive together.",
+                692.142,
+                "/tbVZ3Sq88dZaCANlUcewQuHQOaE.jpg",
+                "2020-11-25",
+                135076090,
+                "Released",
+                7.5,
+                1791, false
+            )
+        )
+    }
+
     fun getMovieById(id: Int): Movie? = generateMovies().find { it.id == id }
 
     fun getTvShowById(id: Int): TvShow? = generateTvShows().find { it.id == id }
+
+    fun getTrendingById(id: Int): Trending? = generateTrendings().find { it.id == id }
 }
